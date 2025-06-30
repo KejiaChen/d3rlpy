24c0cb6999f116697b2e28dde1bafaf573f63b02
KejiaChen <kejia.chen@tum.de>
2025-06-30 12:45:08 +0200

    export onnx and align observation dimesnion (B,T,obs_dim)

--- examples/train_ciip_fixing_episodewise.py
+++ examples/train_ciip_fixing_episodewise.py
@@ -16,6 +16,7 @@ import wandb
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+from d3rlpy.dataset import EpisodeWindowDataset
 
 
 class MLPPolicy(nn.Module):
@@ -27,9 +28,14 @@ class MLPPolicy(nn.Module):
             layers += [nn.Linear(in_dim, out_dim), nn.ReLU()]
         self.encoder = nn.Sequential(*layers)
         self.head = nn.Linear(dims[-1], act_dim)
-
-    def forward(self, x):  # x: (T, obs_dim)
-        return self.head(self.encoder(x))  # (T, act_dim)
+    
+    def forward(self, x):  # x: (T, obs_dim) or (B, T, obs_dim)
+        if x.dim() == 3:
+            B, T, D = x.shape
+            x = x.view(B * T, D)
+            out = self.head(self.encoder(x))
+            return out.view(B, T, -1)  # restore batch + seq
+        return self.head(self.encoder(x))
 
 def collate_variable_episodes(batch):
     """
@@ -140,12 +146,35 @@ def evaluate_imitation_policy(policy, dataset):
     print(f"Average MSE over dataset: {avg_mse:.4f}")
     return avg_mse
 
+def export_to_onnx(policy, obs_dim, export_path="mlp_policy.onnx"):
+    policy.eval()
+    dummy_input = torch.randn(1, 1, obs_dim)  # 3D input: (batch_size, seq_len, obs_dim) to match the expected input shape in cpp
+    torch.onnx.export(
+        policy, 
+        dummy_input, 
+        export_path,
+        export_params=True,
+        opset_version=11,
+        input_names=["input"],
+        output_names=["output"],
+        dynamic_axes={
+            "input": {0: "batch_size"},  # support variable batch size
+            "output": {0: "batch_size"}
+        }
+    )
+    print(f"ONNX model exported to {export_path}")
+
 
 if __name__ == "__main__":
+    train = True # Set to False to skip training and only evaluate
+    update_step = 5  # update the policy output every 5 robot control loops, i.e. 200Hz for the 1000Hz robot control frequency
+
+    '''prepare the dataset'''
     reload_data=False
 
-    base_dir = "/home/tp2/Documents/kejia/clip_fixing_dataset/"
-    dataset = load_trajectories(base_dir, env_step=5, reload_data=reload_data)
+    dataset_base_dir = "/home/tp2/Documents/kejia/clip_fixing_dataset/"
+    dataset = load_trajectories(dataset_base_dir, env_step=update_step, reload_data=reload_data)
+    seq_dataset =  EpisodeWindowDataset(dataset, seq_len=5)  # seq_len=5 for 5-step window
 
     from torch.utils.data import random_split
     # 80% train, 20% validation
@@ -160,24 +189,61 @@ if __name__ == "__main__":
     print(f"Training using dataset with {dataset.__len__()} episodes, "
           f"obs_dim={obs_dim}, act_dim={act_dim}")
 
-    policy = train_weighted_bc(
-                            train_dataset,
-                            obs_dim=obs_dim,
-                            act_dim=act_dim,
-                            num_epochs=50,
-                            batch_size=1,
-                            lr=1e-3
-                        )
+    '''train the policy'''
+    if train:
+        policy = train_weighted_bc(
+                                train_dataset,
+                                obs_dim=obs_dim,
+                                act_dim=act_dim,
+                                num_epochs=50,
+                                batch_size=1,
+                                lr=1e-3
+                            )
+    
+    log_base_dir = "/home/tp2/Documents/kejia/d3rlpy/d3rlpy_logs/Clip_Weighted_BC/"
+    if train:
+        log_dir = os.path.join(log_base_dir, time.strftime("%Y%m%d_%H%M%S"))
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir, exist_ok=True)
+    else:
+        log_dir = os.path.join(log_base_dir, "20250629_170553")
 
-    # offline imitation eval
-    policy.eval()
+    policy_path = os.path.join(log_dir, "trained_BC_policy.pth")
+
+    if train:
+        # save the policy
+        torch.save(policy.state_dict(), policy_path)
+        export_to_onnx(policy, obs_dim, export_path=os.path.join(log_dir, "trained_BC_policy.onnx"))
+
+    '''Evaluate the policy'''
+    # load the policy
+    reload_policy = MLPPolicy(obs_dim, act_dim)
+    reload_policy.load_state_dict(torch.load(policy_path))
+    reload_policy.eval()
+    # offline imitation evaluation
     with torch.no_grad():
          for idx, (obs_seq, act_seq, weight) in enumerate(val_dataset):  # obs_seq: (T, obs_dim)
-            pred_seq = policy(obs_seq)  # (T, act_dim), works for MLP
+            # # evaluate the policy episode-wise
+            # pred_seq = reload_policy(obs_seq)  # (T, act_dim), works for MLP
+            # mse = F.mse_loss(pred_seq, act_seq).item()
+            # print(f"Evaluation MSE: {mse:.4f} for episode with weight {weight:.4f}")
+
+            # evaluate the policy step by step
+            pred_seq = []
+            env_count = 0
+            pred_step = torch.zeros(1, act_seq.shape[1])  # Initialize pred_step with zeros
+
+            for i in range(obs_seq.shape[0]):
+                obs_step = (obs_seq[i].unsqueeze(0)).unsqueeze(0)
+                if env_count % update_step == 0:  # only update the output every `update_step` steps
+                    pred_step = reload_policy(obs_step)  # (1, act_dim)
+                pred_seq.append(pred_step.squeeze(0))
+                env_count += 1
+            pred_seq = torch.stack(pred_seq)  # shape: (T, act_dim)
             mse = F.mse_loss(pred_seq, act_seq).item()
             print(f"Evaluation MSE: {mse:.4f} for episode with weight {weight:.4f}")
 
-     # === Plot Actions: Ground Truth vs Prediction ===
+            # === Plot Actions: Ground Truth vs Prediction ===
             T, act_dim = act_seq.shape
             fig, axs = plt.subplots(act_dim, 1, figsize=(10, 2 * act_dim), sharex=True)
             if act_dim == 1:
@@ -186,14 +252,14 @@ if __name__ == "__main__":
             # plot stertch
             axs[0].plot(range(T), obs_seq[:, 0].cpu().numpy(), label="Obs", color="green")
             axs[0].plot(range(T), act_seq[:, 0].cpu().numpy(), label="Demo_act", color="red")
-            axs[0].plot(range(T), pred_seq[:, 0].cpu().numpy(), label="Pred", color="red", linestyle="--")
+            axs[0].plot(range(T), pred_seq[:, 0, 0].cpu().numpy(), label="Pred", color="red", linestyle="--")
             axs[0].set_ylabel(f"Stretch")
             axs[0].legend()
 
             axs[1].plot(range(T), obs_seq[:, 3].cpu().numpy(), label="Obs", color="green")
             axs[1].plot(range(T), act_seq[:, 1].cpu().numpy(), label="Demo_act", color="red")
-            axs[1].plot(range(T), pred_seq[:, 1].cpu().numpy(), label="Pred", color="red", linestyle="--")
-            axs[1].set_ylabel(f"Stretch")
+            axs[1].plot(range(T), pred_seq[:, 0, 1].cpu().numpy(), label="Pred", color="red", linestyle="--")
+            axs[1].set_ylabel(f"PUsh")
             axs[1].legend()
 
             axs[-1].set_xlabel("Timestep")
